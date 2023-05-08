39fa13e16c56acd2283cb4fe81d3ec2a2828f288
Mahamud Aden <mahamudadn@gmail.com>
2023-05-08 14:51:02 -0500

    craeted onraedy function

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 
 console.log('Jquery is ruuning')
 
-// $(document).ready(onReady);
+ $(document).ready(onReady);
 
-// function onReady() {
-// console.log('Jquery is running')
-//     // called my id of the botton and told jquery to do something when ever click hsppens.
-//     $('#button').on('click', genereteButton)
+// craeted an event tester
 
-// }
+ function onReady() {
+    $('#button').on('click', handleClick)
+ }
 
-
-
-// // created a funtion that will put things in my DOM
-
-// function genereteButton() {
-// console.log('Generate Click')
-
-//     $('#button').append('<div></div>')
-
-// }
+// hanlde click function that will deal whenever someone clicks the generate button.
+ function handleClick(event) {
+    console.log('Click Generate')
+ }
