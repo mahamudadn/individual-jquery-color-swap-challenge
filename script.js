3fa3091c85ec735368d105042044854f723c2a78
Mahamud Aden <mahamudadn@gmail.com>
2023-05-08 14:07:03 -0500

    created generatebutton funtion to append my DOM

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 $(document).ready(onReady);
 
 function onReady() {
-    
+
+    // called my id of the botton and told jquery to do something when ever click hsppens.
+    $('#button').on('click', genereteButton)
+
+}
+
+
+
+// created a funtion that will put things in my DOM
+
+function genereteButton() {
 
 }
