23fc59167a24898896c31bf002ce7fbcbabc3e35
Mahamud Aden <mahamudadn@gmail.com>
2023-05-08 15:17:15 -0500

    changed the background of my generate toi red

--- script.js
+++ script.js
@@ -15,11 +15,12 @@ let clickCount = 0;
 // hanlde click function that will deal whenever someone clicks the generate button.
  function handleClick(event) {
     console.log('Click Generate')
-    
+
 // this funtion will add one more whenever i click the button
     clickCount++;
 
     // Targeted this id to be incremented
 
-    $('#clickCounter').text(clickCount)
+    $('#clickCounter').text(clickCount).css('background-color', 'red')
+    // $('#button').css('background-color', 'red')
  }
