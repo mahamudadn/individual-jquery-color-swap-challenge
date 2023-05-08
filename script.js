60bdb5a37df3ac872a2ad46ce81e13556b66baeb
Mahamud Aden <mahamudadn@gmail.com>
2023-05-08 14:30:17 -0500

    tested my console log

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-$(document).ready(onReady);
 
-function onReady() {
+console.log('Jquery is ruuning')
 
-    // called my id of the botton and told jquery to do something when ever click hsppens.
-    $('#button').on('click', genereteButton)
+// $(document).ready(onReady);
 
-}
+// function onReady() {
+// console.log('Jquery is running')
+//     // called my id of the botton and told jquery to do something when ever click hsppens.
+//     $('#button').on('click', genereteButton)
+
+// }
 
 
 
-// created a funtion that will put things in my DOM
+// // created a funtion that will put things in my DOM
 
-function genereteButton() {
-console.log('Generate Click')
+// function genereteButton() {
+// console.log('Generate Click')
 
-    $('#button').append('<div></div>')
+//     $('#button').append('<div></div>')
 
-}
+// }
