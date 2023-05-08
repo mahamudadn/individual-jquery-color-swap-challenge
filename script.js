bae64de7cb25893c0a0b995f5be4c4161eec08f1
Mahamud Aden <mahamudadn@gmail.com>
2023-05-08 14:54:36 -0500

    craeted an click count variable outside the function and also added clickcount ++ inside the function

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ console.log('Jquery is ruuning')
     $('#button').on('click', handleClick)
  }
 
+let clickCount = 0;
+
+
 // hanlde click function that will deal whenever someone clicks the generate button.
  function handleClick(event) {
     console.log('Click Generate')
+
+    clickCount++;
  }
