2690904cb4fdafbfae847a6d28b6a0bf73008eef
Mahamud Aden <mahamudadn@gmail.com>
2023-05-11 17:14:12 -0500

    created yellow and delete buttons

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ console.log('Jquery is ruuning')
 
  function onReady() {
     $('#button').on('click', handleClick)
+    $('#container').on('click', '.yellowColor', yellowButton)
+    $('#container').on('click', '.deleteIt', yellowButton)
  }
 
 let clickCount = 0;
@@ -17,10 +19,18 @@ let clickCount = 0;
     console.log('Click Generate')
 
 // this funtion will add one more whenever i click the button
-    clickCount++;
+$('#container').append(`
+   <div id ="container"> 
+   <button class ="yellowColor">Yellow</button>
+   <button class ="deleteIt">Delete</button>
+   </div>
+`)
 
     // Targeted this id to be incremented
-
+    clickCount++;
     $('#clickCounter').text(clickCount).css('background-color', 'red')
     // $('#button').css('background-color', 'red')
  }
+function yellowButton() {
+   $(this).parent().css('background-color', 'yellow');
+}
